0c665e7b9fd1d69549fb05c732c70732a6ea4003
Sahil <Sahil@ameotech.com>
2023-10-18 19:41:06 +0530

    Model

--- src/components/SignupModal/SignUpModal.js
+++ src/components/SignupModal/SignUpModal.js
@@ -23,45 +23,39 @@ const SignUpModal = ({ religionData, modalShow, handleRegister, communitiesData,
                                     <option value={item.name} key={item.id}>{item.name}</option>
                                 )
                             })}
-                            {/* <option value="Hindu" id="1">Hindu</option>
-                            <option value="Islam" id="2">Islam</option>
-                            <option value="Sikh" id="3">Sikh</option>
-                            <option value="Islam" id="4">Christian</option>
-                            <option value="Nonreligious" id="5">Nonreligious</option>
-                            <option value="Others" id="5">Others</option> */}
-
                         </select>
                     </div>
                     <div className="col-lg-12 col-md-12 col-sm-6 form-group ">
-                        <select className="custom-select-box" name="community" tabindex="7" required id="commnity" onChange={(e) => handleRegister(e)}>
+                        <select className="custom-select-box" name="community" tabindex="8" required id="commnity" onChange={(e) => handleRegister(e)}>
                             <option value="" disabled selected hidden>Community</option>
                             {!!communitiesData?.length && communitiesData.map((item) => {
                                 return (
-                                    <option value={item.nam}>{item.name}</option>
+                                    <option value={item.name}>{item.name}</option>
                                 )
                             })}
                         </select>
                     </div>
 
                     <div className="col-lg-12 col-md-12 col-sm-6 form-group ">
-                        <select className="custom-select-box" name="familyName" tabindex="7" required id="family" onChange={(e) => handleRegister(e)}>
+                        <select className="custom-select-box" name="familyName" tabindex="9" required id="family" onChange={(e) => handleRegister(e)}>
                             <option value="" disabled selected hidden>Family Name</option>
                             {!!familyNameData?.length && familyNameData.map((item) => {
                                 return (
-                                    <option value={item.nam}>{item.name}</option>
+                                    <option value={item.name}>{item.name}</option>
                                 )
                             })}
                         </select>
                     </div>
 
                     <div className="col-lg-12 col-md-12 col-sm-6 form-group ">
-                        <select className="custom-select-box" name="living_in" tabindex="7" required id="month" onChange={(e) => handleRegister(e)}>
+                        <select className="custom-select-box" name="living_in" tabindex="10" required id="month" onChange={(e) => handleRegister(e)}>
                             <option value="" disabled selected hidden>Living in</option>
                             {countries.map((item) => {
                                 return <option value={item.name} key={item.code}>{item.name}</option>
                             })}
                         </select>
                     </div>
+
                 </div>
             </Modal.Body>
         </Modal>
